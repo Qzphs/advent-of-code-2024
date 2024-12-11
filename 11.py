7e6972d39cbd0fd4d9e869cb0a63644eb6c298f1
Qzphs <86579079+Qzphs@users.noreply.github.com>
2024-12-11 18:25:45 +1300

    Complete Day 11

---
+++ 11.py
@@ -0,0 +1,40 @@
+class StoneCollection:
+
+    def __init__(self):
+        self.stones: dict[str, int] = {}
+
+    @classmethod
+    def from_input(cls, data: str):
+        result = StoneCollection()
+        for stone in data.split():
+            result.add(stone, 1)
+        return result
+
+    def add(self, stone: str, amount: int):
+        if stone not in self.stones:
+            self.stones[stone] = 0
+        self.stones[stone] += amount
+
+    def blink(self):
+        result = StoneCollection()
+        for stone, amount in self.stones.items():
+            if stone == "0":
+                result.add("1", amount)
+            elif len(stone) % 2 == 0:
+                left = str(int(stone[: len(stone) // 2]))
+                right = str(int(stone[len(stone) // 2 :]))
+                result.add(left, amount)
+                result.add(right, amount)
+            else:
+                multiplied = str(int(stone) * 2024)
+                result.add(multiplied, amount)
+        return result
+
+    def total(self):
+        return sum(self.stones.values())
+
+
+stones = StoneCollection.from_input(input())
+for _ in range(25):
+    stones = stones.blink()
+print(stones.total())
